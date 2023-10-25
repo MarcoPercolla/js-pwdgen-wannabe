82d6f5ba8e8a75ead5897c5062200387085bc8f4
ReDMallard <marco@titori.it>
2023-10-26 00:58:11 +0200

    minor

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const nome = prompt( "Inserisci il tuo nome", "Gollum");
 const cognome = prompt( "Inserisci il tuo cognome", "Smeagol");
 const colore = prompt( "Inserisci il tuo colore preferito", "tesssoro");
+const password = `${nome}${cognome}${colore}`;
 
 
-document.getElementById("password").innerHTML = `La tua password è:  ${nome}${cognome}${colore}#23`;
+document.getElementById("password").innerHTML = `La tua password è:  ${password}#23`;
